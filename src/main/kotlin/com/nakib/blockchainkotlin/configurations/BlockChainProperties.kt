71407bf75086306d06358cc7e67884c50132f11b
nalamkhan <nalamkhan@hy-vee.com>
2021-03-16 23:32:43 -0500

    (refactor) blockchain validity

---
+++ src/main/kotlin/com/nakib/blockchainkotlin/configurations/BlockChainProperties.kt
@@ -0,0 +1,7 @@
+package com.nakib.blockchainkotlin.configurations
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+
+@ConfigurationProperties("blockchain")
+class BlockChainProperties {
+}
--- src/main/kotlin/com/nakib/blockchainkotlin/services/BlockChain.kt
+++ src/main/kotlin/com/nakib/blockchainkotlin/services/BlockChain.kt
@@ -6,7 +6,12 @@ class BlockChain    {
     private var blocks: MutableList<Block> = mutableListOf()
 
     fun add(block: Block) : Block {
-        val minedBlock = if(block.isMined()) return block else mine(block)
+        val minedBlock = if(block.isMined())    {
+            return block
+        } else  {
+            mine(block)
+        }
+
         blocks.add(minedBlock)
         return minedBlock
     }
@@ -22,5 +27,31 @@ class BlockChain    {
         println("Mined : $minedBlock")
 
         return minedBlock
+    }
+
+    fun isValid() :Boolean  {
+        if(this.blocks.size == 0) {
+            println("BlockChain is empty")
+            return true
+        }
+
+        if(this.blocks.size == 1) {
+            println("BlockChain has only one item")
+            return blocks[0].hash == blocks[0].calculateHash()
+        }
+
+        for (i in 1 until blocks.size) {
+            val previousBlock = blocks[i - 1]
+            val currentBlock = blocks[i]
+
+            when {
+                currentBlock.hash != currentBlock.calculateHash() -> return false
+                previousBlock.hash != previousBlock.calculateHash() -> return false
+                !(previousBlock.isMined() && currentBlock.isMined()) -> return false
+            }
+        }
+
+        println("The Block Chain is Valid")
+        return true
     }
 }
